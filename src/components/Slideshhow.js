9d9a884ba551a4504b7767d835a94aecfd9d284a
nilesh7808 <rajnilesh088@gmail.com>
2022-12-01 21:18:24 +0530

    React WebApp

---
+++ src/components/Slideshhow.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import gym1 from '../images/gym1.jpg';
+import gym2 from '../images/gym2.jpg';
+import gym3 from '../images/gym3.jpg';
+import slideshow from '../css/slideshow.css';
+
+const Slideshhow = () => {
+    return (
+        <div id="caraousel">
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={gym1} className="d-block w-100" alt="Gym One Photo..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={gym2} className="d-block w-100" alt="Gym Two Photo..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={gym3} className="d-block w-100" alt="Gym Three Photo..." />
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Slideshhow
