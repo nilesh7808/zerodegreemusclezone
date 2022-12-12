fbf27eb1ee0099f1c459e89e8de355512c8edc48
nilesh7808 <rajnilesh088@gmail.com>
2022-12-12 18:05:57 +0530

    For production and deployment

--- src/components/Slideshhow.js
+++ src/components/Slideshhow.js
@@ -6,30 +6,28 @@ import slideshow from '../css/slideshow.css';
 
 const Slideshhow = () => {
     return (
-        <div>
-                <div id="caraousel">
-                    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src={gym1} className="d-block w-100" alt="Gym One Photo..." />
-                            </div>
-                            <div className="carousel-item">
-                                <img src={gym2} className="d-block w-100" alt="Gym Two Photo..." />
-                            </div>
-                            <div className="carousel-item">
-                                <img src={gym3} className="d-block w-100" alt="Gym Three Photo..." />
-                            </div>
-                        </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
+        <div id="caraousel">
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={gym1} className="d-block w-100" alt="Gym One Photo..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={gym2} className="d-block w-100" alt="Gym Two Photo..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={gym3} className="d-block w-100" alt="Gym Three Photo..." />
                     </div>
                 </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
     )
 }
